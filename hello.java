ebfc63c9bf7f75dcc11ca039195298751f01dc38
Sophiakhan123 <sophiakhan2306@gmail.com>
2026-04-01 11:35:34 +0530

    updated code

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 public class hello{
     public static void main(String []args){
         System.out.println("hello");
+        int a =5,b=10;
+        in s =a+b;
+        System.out.println("sum: "+s);
     }
 }
